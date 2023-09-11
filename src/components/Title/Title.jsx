bac8381891299f45d41011e56747f00d9fc1147e
Marwan Elsayed <marwanellsayed@hotmail.com>
2023-09-12 01:52:46 +0300

    update

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 export default function Title() {
-  const title = window.location.pathname;
+  const title = window.location.href.split("/")[4];
+  console.log(title);
   return (
     <>
       <h1 className="text-uppercase mb-3 fw-bolder fs-1">
-        {title === "/" ? "start framework" : `${title.slice(1)} COMPONENT`}
+        {title === "" ? "start framework" : `${title} COMPONENT`}
       </h1>
     </>
   );
